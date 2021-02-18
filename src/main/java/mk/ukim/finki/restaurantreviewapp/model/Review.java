c840001b4c94faa754a3d1d14edf490ad1f7c454
marijacvetanoska <cvetanoska.marija01@gmail.com>
2021-02-18 18:40:56 +0100

    review class added

--- src/main/java/mk/ukim/finki/restaurantreviewapp/model/Review.java
+++ src/main/java/mk/ukim/finki/restaurantreviewapp/model/Review.java
@@ -1,4 +1,18 @@
 package mk.ukim.finki.restaurantreviewapp.model;
 
+import lombok.Data;
+
+@Data
 public class Review {
+    private float rating;
+    private String comment;
+
+    public Review(float rating) {
+        this.rating = rating;
+    }
+
+    public Review(float rating, String comment) {
+        this.rating = rating;
+        this.comment = comment;
+    }
 }
